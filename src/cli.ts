f304af5206a43f040f5340faad0a921a9a97134a
Misha Soliterman <msoliter@gmail.com>
2020-12-13 19:32:17 +0900

    new: Some notes on the CLI.

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,25 @@
 import program from 'commander'
+import { IndentationText, NewLineKind, Project, QuoteKind } from 'ts-morph'
 
 program
-  .name('vue-classify')
+  .name('vue-declassify')
   .version('1.0.0')
-  .parse(process.argv)
+  .command('transform <path>')
+  .description('Transforms the components of the target project.', {
+    path: 'The path of the project (must have a tsconfig.json).',
+  })
+  .action(path => {
+    const project = new Project({
+      tsConfigFilePath: `${path}/tsconfig.json`,
+      manipulationSettings: {
+        newLineKind: NewLineKind.LineFeed,
+        quoteKind: QuoteKind.Single,
+        useTrailingCommas: false,
+        indentationText: IndentationText.TwoSpaces,
+      },
+    })
 
-console.log(program.helpInformation())
+    // Then presumably use this project to produce source files for declassify().
+    console.log(project.getSourceFiles().map(file => file.getFilePath()))
+  })
+  .parse(process.argv)
